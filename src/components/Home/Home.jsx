f496fd4ac8e5ad24774600aa6342e939223c8696
Abhishek Verma <vermaabhimanyu593@gmail.com>
2024-07-04 22:23:11 +0530

    2nd phase completed

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -9,7 +9,14 @@ import {
   faChartLine,
   faComment,
 } from "@fortawesome/free-solid-svg-icons";
-import Home2 from "../../assets/home2.jpeg"
+import {
+  faUsers,
+  faBuilding,
+  faCalendarCheck,
+  faMoneyBillWave,
+} from "@fortawesome/free-solid-svg-icons";
+
+import Home2 from "../../assets/home2.jpeg";
 function Home() {
   return (
     <>
@@ -90,33 +97,36 @@ function Home() {
           </p>
           <div className="flex md:justify-center justify-around p-4 md:space-x-36 md:p-4 flex-wrap">
             <div className="card w-64 p-4 flex justify-center flex-col text-center h-fit ">
-              <FontAwesomeIcon
-                icon={faUser}
-                className="text-4xl p-2"
-              />
-              <p className="text-D_Grey font-bold text-xl">Membership<br />Organisations</p>
+              <FontAwesomeIcon icon={faUser} className="text-4xl p-2" />
+              <p className="text-D_Grey font-bold text-xl">
+                Membership
+                <br />
+                Organisations
+              </p>
               <p className="text-xs text-Grey">
                 Our membership management software provides full automation of
                 membership renewals and payments
               </p>
             </div>
             <div className="card w-64 p-4 flex justify-center flex-col text-center h-fit ">
-              <FontAwesomeIcon
-                icon={faUser}
-                className="text-4xl p-2"
-              />
-              <p className="text-D_Grey font-bold text-xl">Membership<br />Organisations</p>
+              <FontAwesomeIcon icon={faUser} className="text-4xl p-2" />
+              <p className="text-D_Grey font-bold text-xl">
+                Membership
+                <br />
+                Organisations
+              </p>
               <p className="text-xs text-Grey">
                 Our membership management software provides full automation of
                 membership renewals and payments
               </p>
             </div>
             <div className="card w-64 p-4 flex justify-center flex-col text-center h-fit ">
-              <FontAwesomeIcon
-                icon={faUser}
-                className="text-4xl p-2"
-              />
-              <p className="text-D_Grey font-bold text-xl">Membership<br />Organisations</p>
+              <FontAwesomeIcon icon={faUser} className="text-4xl p-2" />
+              <p className="text-D_Grey font-bold text-xl">
+                Membership
+                <br />
+                Organisations
+              </p>
               <p className="text-xs text-Grey">
                 Our membership management software provides full automation of
                 membership renewals and payments
@@ -124,19 +134,97 @@ function Home() {
             </div>
           </div>
         </div>
-        <div className="flex justify-center p-4 border-2 border-black">
-          <div className="for-home2 flex md:w-1/2">
+
+        <div className="flex justify-center p-4 md:p-12">
+          <div className="for-home2 md:gap-16 flex md:w-1/2">
             <div className="hidden md:block">
               <img src={Home2} alt="home2 photo" className="w-full" />
             </div>
             <div className="flex gap-2 md:gap-4 flex-col">
-              <p className="text-start text-D_Grey text-xl md:text-3xl font-bold">The unseen of spending three years at Pixelgrade</p>
-              <p className="text-start text-Grey text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet justo ipsum. Sed accumsan quam vitae est varius fringilla. Pellentesque placerat vestibulum lorem sed porta. Nullam mattis tristique iaculis. Nullam pulvinar sit amet risus pretium auctor. Etiam quis massa pulvinar, aliquam quam vitae, tempus sem. Donec elementum pulvinar odio.</p>
-              <button className="text-start w-fit pl-4 pr-4 pt-2 pb-2 bg-customGreen text-white">Learn More</button>
+              <p className="text-start text-D_Grey text-xl md:text-3xl font-bold">
+                The unseen of spending three years at Pixelgrade
+              </p>
+              <p className="text-start text-Grey text-sm">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit
+                amet justo ipsum. Sed accumsan quam vitae est varius fringilla.
+                Pellentesque placerat vestibulum lorem sed porta. Nullam mattis
+                tristique iaculis. Nullam pulvinar sit amet risus pretium
+                auctor. Etiam quis massa pulvinar, aliquam quam vitae, tempus
+                sem. Donec elementum pulvinar odio.
+              </p>
+              <button className="text-start w-fit pl-4 pr-4 pt-2 pb-2 bg-customGreen text-white">
+                Learn More
+              </button>
             </div>
           </div>
         </div>
-      </div>
+</div>
+        {/* new start */}
+        <div className="bg-silver">
+          <div className="w-[80%] mx-auto flex  flex-col md:flex-row md:justify-around pt-12 pb-12">
+            <div className=" h-fit p-2 md:mt-16 ">
+              <p className="text-3xl">
+                <span className="text-D_Grey">Helping a local</span>
+                <br />
+                <span className="text-customGreen">
+                  business reinvent itself
+                </span>
+              </p>
+              <p className="text-Grey text-xs font-bold">
+                We reached here with our hard work and dedication
+              </p>
+            </div>
+            <div className="grid grid-rows-2 grid-cols-2">
+              {" "}
+              <div className="sm-cards md:p-4 flex gap-2 md:gap-4">
+                {" "}
+                <FontAwesomeIcon
+                  icon={faUsers}
+                  className="text-2xl p-2 text-customGreen"
+                />
+                <div className="text-sm md:text-lg">
+                  <p>2,245,341</p>
+                  <p>Members</p>
+                </div>
+              </div>
+              <div className="sm-cards md:p-4 flex gap-2 md:gap-4">
+                <FontAwesomeIcon
+                  icon={faBuilding}
+                  className="text-2xl p-2  text-customGreen"
+                />
+
+                <div className="text-sm md:text-lg">
+                  <p>46,328</p>
+                  <p>Clubs</p>
+                </div>
+              </div>
+              <div className="sm-cards md:p-4 flex gap-2 md:gap-4">
+                <FontAwesomeIcon
+                  icon={faCalendarCheck}
+                  className="text-2xl p-2 text-customGreen"
+                />
+
+                <div className="text-sm md:text-lg">
+                  <p>828,867</p>
+                  <p>Event Bookings</p>
+                </div>
+              </div>
+              <div className="sm-cards md:p-4 flex gap-2 md:gap-4">
+                <FontAwesomeIcon
+                  icon={faMoneyBillWave}
+                  className="text-2xl p-2 text-customGreen"
+                />
+
+                <div className="text-sm md:text-lg">
+                  <p>1,926,436</p>
+                  <p>Payments</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        
     </>
   );
 }
